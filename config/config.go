2198e63898c0372fb22b9c5d15dc9736ec1d4b32
leon-i <isakleon97@gmail.com>
2020-11-16 21:13:14 -0500

    Updated gitignore + removed comments

--- config/config.go
+++ config/config.go
@@ -23,9 +23,8 @@ func SetValues(quality uint, destination string) {
 	} else {
 		SquishConfig.Destination = destination
 	}
-
-	//fmt.Println(fmt.Sprintf("\nimage quality set to %d%%", SquishConfig.Quality))
-	fmt.Println("\nimage quality set to", &SquishConfig.Quality)
+	
+	fmt.Println("\nimage quality set to", SquishConfig.Quality)
 	fmt.Println("\nimage(s) will be output to ./" + SquishConfig.Destination)
 	fmt.Println("")
 }
